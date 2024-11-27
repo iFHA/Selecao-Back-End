a4075c97efa901ae86c1980fa79504d6b9e079b6
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-27 02:29:23 -0300

    add requests

---
+++ app/Http/Requests/Auth/ChangePasswordRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class ChangePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'current_password' => 'required|max:255',
+            'new_password' => ['required', 'confirmed', Password::defaults()]
+        ];
+    }
+}
---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique(User::class)
+            ],
+            'password' => ['required', 'confirmed', Password::defaults()]
+        ];
+    }
+}
