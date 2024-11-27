a6cc01a7efa053ced1e4eba05fc1df76fcd72336
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-27 02:31:52 -0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/comments', [CommentController::class, 'findAll']);
+Route::get('/comments/{commentId}', [CommentController::class, 'findById'])->name('comment.show');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::patch('/changePassword', [AuthController::class, 'changePassword']);
+
+    Route::put('/me', [AuthController::class, 'updateMe'])->name('user.update');
+
+    // ver historico somente dono do recurso
+    Route::get('/comments/{commentId}/history', [CommentController::class, 'history']);
+    Route::post('/comments', [CommentController::class, 'create']);
+    // somente se for o dono do recurso
+    Route::put('/comments/{commentId}', [CommentController::class, 'update']);
+    // somente se admin
+    Route::delete('/comments', [CommentController::class, 'deleteAll']);
+    // somente se for o dono do recurso
+    Route::delete('/comments/{commentId}', [CommentController::class, 'delete']);
 });
